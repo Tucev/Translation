9d558905cdc9eeab1962664ed2b9d8dd032888ff
Camille TEROL <camilleterol@gmail.com>
2013-07-13 13:24:23 +0200

    Adds CSS, JS, and title modification in Engine and Controllers

--- classes/engine.php
+++ classes/engine.php
@@ -4,15 +4,29 @@
 		static private $lang = null;
 		static private $content = null;
 		static private $name = null;
+		static private $title = null;
+		static private $css = [];
+		static private $js = [];
 	
 		static function init() {
 			self::$uri = self::parse();
 			self::locale();
+			self::loadCSS();
+			self::loadJS();
+			
+			if(self::$js == null) {
+				self::$js = Config::get("engine.js.files");
+			}
+			
 			if(self::$uri) {
-				self::loadController();
+				require_once(self::getController());
 				
-				$v = self::loadView();
-				$pv = self::processView($v, true);
+				if(!isset($data) || !is_array($data)) {
+					$data = true;
+				}
+				
+				$v = "\n" . self::loadView();
+				$pv = self::processView($v, $data);
 				self::$content = Language::process($pv, self::$lang);
 			}
 			self::render();
@@ -69,7 +83,6 @@
 			
 			return $data;
 		}
-		
 		static function getContent() {
 			if(self::$content != null) {
 				return self::$content;
@@ -78,40 +91,72 @@
 			}
 		}
 		
+		static function addCSS($file) {
+			array_push(self::$css, Config::get("paths.webroot") . Config::get("engine.css.path") . $file);
+		}
 		static function getCSS() {
-			$css = glob(Config::get("paths.webroot").Config::get("engine.css.paths").Config::get("engine.css.wildcard"));
-			return false;	
+			self::loadCSS();
+			$css = glob(Config::get("paths.webroot") . Config::get("engine.css.path") . Config::get("engine.css.wildcard"));
+			$css = array_intersect(self::$css, $css);
+			$cv = self::loadView("css");
+			
+			foreach($css as $key => $path) {
+				$reg = "/" . preg_quote(Config::get("paths.webroot"), "/") . "/";
+				$pcv = self::processView($cv, [ "path" => preg_replace($reg, "/", $path) ]);
+				$css[$key] = "\n" . $pcv;
+			}
+			return implode("", $css);	
 		}
-		
-		static function setCSS() {
-			return false;	
-		}
-		
-		static function addCSS() {
-			return false;
+		static function loadCSS() {
+			if(self::$css == null) {
+				$css = Config::get("engine.css.files");
+				foreach($css as $file) {
+					self::addCSS($file);
+				}
+			}
 		}
 		
 		static function getTitle() {
+			if(self::$title != null) { 
+				$separator = Config::get("engine.title.separator");
+				$title = $separator . self::$title;
+				return $title;
+			}
 			return false;
 		}
-		
-		static function setTitle() {
-			return false;
+		static function setTitle($title) {
+			self::$title = Language::process($title, self::$lang);
 		}
 		
+		static function addJS($file) {
+			array_push(self::$js, Config::get("paths.webroot") . Config::get("engine.js.path") . $file);
+		}
 		static function getJS() {
-			return false;
+			self::loadJS();
+			$js = glob(Config::get("paths.webroot") . Config::get("engine.js.path") . Config::get("engine.js.wildcard"));
+			$js = array_intersect(self::$js, $js);
+			$jv = self::loadView("js");
+			
+			foreach($js as $key => $path) {
+				$reg = "/" . preg_quote(Config::get("paths.webroot"), "/") . "/";
+				$pjv = self::processView($jv, [ "path" => preg_replace($reg, "/", $path) ]);
+				$js[$key] = "\n" . $pjv;
+			}
+			return implode("", $js);
 		}
-		
-		static function setJS() {
-			return false;
+		static function loadJS() {
+			if(self::$js == null) {
+				$js = Config::get("engine.js.files");
+				foreach($js as $file) {
+					self::addJS($file);
+				}
+			}
 		}
-		
-		static function addJS() {
-			return false;
-		}
-		
+
 		static function loadController($name = false) {
+			require_once(self::getController($name));
+		}
+		static function getController($name = false) {
 			if($name == false) {
 				$name = self::$uri;
 			} else {
@@ -119,10 +164,14 @@
 			}
 			
 			$path = Config::get("paths.controllers") . $name . ".php";
+			echo $path;
 			if(file_exists($path)) {
-				require_once($path);
+				return $path;
+			} else {
+				return false;
 			}
 		}
+		
 		static function loadView($name = false) {
 			if($name == false) {
 				$name = self::$uri;
--- controllers/login.php
+++ controllers/login.php
@@ -1,12 +1,18 @@
 <?php
+	Engine::setTitle("{{ lgs.login.title }}");
+	Engine::addCSS("login.css");
+	Engine::addJS("login.js");
+
+	$data["login"] = array();
+	$login = &$data["login"];
 	// if any information is sent unssing the POST method to the website
 	if($_POST) {
-		echo "Okay, somebody is trying to login !";
+		$login["status"] = "{{ lgs.login.status.post_sent }}";
 		// Check that none of the login or password are empty
 		if(!empty($_POST['password']) && !empty($_POST['login'])) {
-			echo "Login and password present ... Let's keep going";
+			$login["status"] = "{{ lgs.login.status.credentials_entered }}";
 		} else {
-			exit(0);
+			$login["status"] = "{{ lgs.login.status.no_credentials }}";
 		}
 		// Hash password
 		// Check if it matches a record in our database
